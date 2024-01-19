9c7c73fbfb167873d17aa424db787ca9cb57f594
jalland <jjalland@gmail.com>
2024-01-18 21:32:57 -0500

    Edited event handler for the survey submit

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,27 +1,29 @@
-const loginFormHandler = async (event) => {
-    event.preventDefault();
+//WE want to implement a POST request for temperature preference data for the Survey model. 
+
+// const loginFormHandler = async (event) => {
+//     event.preventDefault();
   
-    // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+//     // Collect values from the login form
+//     const email = document.querySelector('#email-login').value.trim();
+//     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
-      // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
+//     if (email && password) {
+//       // Send a POST request to the API endpoint
+//       const response = await fetch('/api/users/login', {
+//         method: 'POST',
+//         body: JSON.stringify({ email, password }),
+//         headers: { 'Content-Type': 'application/json' },
+//       });
   
-      if (response.ok) {
-        // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
+//       if (response.ok) {
+//         // If successful, redirect the browser to the profile page
+//         document.location.replace('/profile');
+//       } else {
+//         alert(response.statusText);
+//       }
+//     }
+//   };
 
   document
-  .querySelector('.login-form')
+  .querySelector('.roommate-survey')
   .addEventListener('submit', loginFormHandler);
